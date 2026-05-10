3a3131e9cb28a03157010c21a7d6588581ae7466
ahuglajbclajep <ahuglajbclajep@gmail.com>
2026-05-10 14:19:30 +0900

    tiptap を更新

--- src/editor-nodes/marks/Text.tsx
+++ src/editor-nodes/marks/Text.tsx
@@ -2,7 +2,7 @@ import { Node } from "@tiptap/core";
 
 /**
  * @see https://www.tiptap.dev/api/nodes/text
- * @see https://github.com/ueberdosis/tiptap/blob/v3.17.1/packages/extension-text/src/text.ts
+ * @see https://github.com/ueberdosis/tiptap/blob/v3.23.1/packages/extension-text/src/text.ts
  */
 export const Text = Node.create({
   name: "text",
