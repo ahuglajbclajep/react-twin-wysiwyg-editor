db712616bfb0e86d4618f88d232b316aa34c4bea
ahuglajbclajep <ahuglajbclajep@gmail.com>
2026-05-10 13:45:41 +0900

    ファイルを移動

---
+++ src/editor-node/extensions.ts
@@ -0,0 +1,7 @@
+import { Document } from "./Document";
+import { Paragraph } from "./Paragraph";
+import { Text } from "./marks/Text";
+
+export const nodes = [Document, Paragraph];
+
+export const marks = [Text];
